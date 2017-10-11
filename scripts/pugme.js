b24f173656d499f2e44d17548bed854c444e4f06
Michael Stephens <michael.stephens@procore.com>
2017-10-11 16:38:38 +0000

    prettier

--- scripts/pugme.js
+++ scripts/pugme.js
@@ -11,21 +11,22 @@
 //   randbot pug me - Receive a pug
 //   randbot pug bomb N - get N pugs (max 20)
 
-module.exports = function(robot) {
-  robot.respond(/pug me/i, function(msg) {
-    return msg.http("http://pugme.herokuapp.com/random").get()(function(err, res, body) {
-      return msg.send(JSON.parse(body).pug);
-    });
-  });
-  robot.respond(/pug bomb( (\d+))?/i, function(msg) {
-    var count;
-    if(parseInt(msg.match[2]) > 20) {
+module.exports = function (robot) {
+  robot.respond(/pug me/i, msg => msg.http('http://pugme.herokuapp.com/random').get()((err, res, body) => msg.send(JSON.parse(body).pug)));
+  robot.respond(/pug bomb( (\d+))?/i, (msg) => {
+    let count;
+    if (parseInt(msg.match[2]) > 20) {
       count = 20;
-    } else {
+    }
+    else {
       count = msg.match[2] || 5;
     }
-    return msg.http("http://pugme.herokuapp.com/bomb?count=" + count).get()(function(err, res, body) {
-      var i, len, pug, ref, results;
+    return msg.http(`http://pugme.herokuapp.com/bomb?count=${count}`).get()((err, res, body) => {
+      let i,
+        len,
+        pug,
+        ref,
+        results;
       ref = JSON.parse(body).pugs;
       results = [];
       for (i = 0, len = ref.length; i < len; i++) {
@@ -35,9 +36,5 @@ module.exports = function(robot) {
       return results;
     });
   });
-  return robot.respond(/how many pugs are there/i, function(msg) {
-    return msg.http("http://pugme.herokuapp.com/count").get()(function(err, res, body) {
-      return msg.send("There are " + (JSON.parse(body).pug_count) + " pugs.");
-    });
-  });
+  return robot.respond(/how many pugs are there/i, msg => msg.http('http://pugme.herokuapp.com/count').get()((err, res, body) => msg.send(`There are ${JSON.parse(body).pug_count} pugs.`)));
 };
