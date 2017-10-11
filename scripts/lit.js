1a450c3090a5cd83f44bf74516f6c84490074de5
Michael Stephens <michael@stephens.tech>
2017-10-10 20:45:08 -0700

    Init lit

---
+++ scripts/lit.js
@@ -0,0 +1,18 @@
+// Description:
+//   a collection of general gamebot functions
+//
+// Dependencies:
+//
+// Configuration:
+//
+// Commands:
+// lit
+//
+// Author:
+//   YashdalfTheGray
+
+module.exports = (robot) => {
+  robot.hear /lit/i, (res) => {
+
+  }
+};
