021440f516fea4859f90dc3db9a94284422f5db5
Michael Stephens <michael@stephens.tech>
2017-10-10 22:20:20 -0700

    TWO BABYEEE

--- scripts/lit.js
+++ scripts/lit.js
@@ -12,14 +12,14 @@
 //   mikestephens/harasho
 
 module.exports = function (robot) {
-    robot.hear(/lit (.*)/i, (res) => {
-        // TODO: make this a configurable value
-        if (res.match[1].includes('https://hashtaggaming.slack.com/archives/')) {
-            res.send('I gotchu fam');
-            robot.messageRoom('lit', res.match[1]);
-        }
-        else {
-            res.send("The fuck you talking 'bout fam?");
-        }
-    });
+  robot.hear(/lit (.*)/i, (res) => {
+    // TODO: make this a configurable value
+    if (res.match[1].includes('https://hashtaggaming.slack.com/archives/')) {
+      res.send('I gotchu fam');
+      robot.messageRoom('lit', res.match[1]);
+    }
+    else {
+      res.send("The fuck you talking 'bout fam?");
+    }
+  });
 };
