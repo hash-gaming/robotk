b210fb1d662b8b7bbe16f6ee31e5c775009059f0
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2017-10-18 20:15:06 -0700

    changed up the async calls to something better, async/await what up

--- scripts/pugme.js
+++ scripts/pugme.js
@@ -8,21 +8,38 @@
 //   None
 //
 // Commands:
-//   randbot pug me - Receive a pug
-//   randbot pug bomb N - get N pugs (max 10)
+//   hubot pug me - Receive a pug
+//   hubot pug bomb N - get N pugs (max 10)
 
-module.exports = function (robot) {
-  robot.respond(/pug me/i, (msg) => {
-    msg.http('http://pugme.herokuapp.com/random').get()((err, res, body) => {
-      msg.send(JSON.parse(body).pug);
-    });
+const request = require('request-promise');
+
+module.exports = (robot) => {
+  robot.respond(/pug me/i, async (msg) => {
+    try {
+      const response = await request({
+        url: 'http://pugme.herokuapp.com/random',
+        json: true
+      });
+      msg.send(response.pug);
+    }
+    catch (err) {
+      msg.send('You dun goofed now!');
+    }
   });
-  robot.respond(/pug bomb( (\d+))?/i, (msg) => {
-    console.log(msg.match[2]);
+
+  robot.respond(/pug bomb( (\d+))?/i, async (msg) => {
     const count = parseInt(msg.match[2], 10) > 10 ? 10 : parseInt(msg.match[2], 10) || 5;
 
-    return msg.http(`http://pugme.herokuapp.com/bomb?count=${count}`).get()((err, res, body) => {
-      JSON.parse(body).pugs.forEach(p => msg.send(p));
-    });
+    try {
+      const response = await request({
+        url: `http://pugme.herokuapp.com/bomb?count=${count}`
+      });
+      response.pugs.forEach(p => msg.send(p));
+    }
+    catch (err) {
+      [...Array(count).keys()]
+      .map(() => 'You dun goofed now!')
+      .forEach(m => msg.send(m));
+    }
   });
 };
