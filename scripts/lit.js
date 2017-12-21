e816058ff704885fdecf1a8eb7c50246b179404e
Rafal Proszowski <paroxp@gmail.com>
2017-12-21 19:01:42 +0000

    Implement "shared content" fam
    Currently a user has to provide robotk with a link to a message that
    they'd like to lit.
    
    We could make it a little smarter and also allow people to share some
    content with the phrase `@robotk lit`.

--- scripts/lit.js
+++ scripts/lit.js
@@ -6,10 +6,15 @@
 // Configuration:
 //
 // Commands:
-// randbot lit <message_link> - Immortalizes a message in #lit... fam
+//   hubot lit <message_link> - Immortalizes a message in #lit... fam
+//   hubot lit - Immortalizes a shared message in #lit... fam
 //
 // Author:
 //   mikestephens/harasho
+
+const _ = require('lodash');
+
+const hasAttachments = rawMessage => _.get(rawMessage, 'attachments.length', 0) > 0;
 
 // TODO: make this a configurable value
 // Just for the time being, let's keep these separate.
@@ -19,10 +24,14 @@ const successMessage = 'I gotchu fam';
 const validURL = 'https://hashtaggaming.slack.com/archives/';
 
 module.exports = (robot) => {
-  robot.respond(/lit (.*)/i, (res) => {
-    const param = res.match[1];
+  robot.respond(/lit\s?(.*)?/i, (res) => {
+    let param = res.match[1];
 
-    if (param.includes(validURL)) {
+    if (hasAttachments(res.message.rawMessage)) {
+      param = res.message.rawMessage.attachments[0].from_url;
+    }
+
+    if (param !== undefined && param.includes(validURL)) {
       res.send(successMessage);
       robot.messageRoom(channel, param);
     }
