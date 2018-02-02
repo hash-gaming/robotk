525a3687b2ad473c6d09747a0747662d4c5f5d17
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-02-01 19:22:22 -0800

    added the new middlewares to the export list

--- middleware/index.js
+++ middleware/index.js
@@ -1,7 +1,11 @@
 const isAdminCheck = require('./isAdmin');
 const verifyIncomingWebhook = require('./verifyWebhook');
+const discuss = require('./discuss');
+const kickEveryone = require('./kickEveryone');
 
 module.exports = {
   isAdminCheck,
-  verifyIncomingWebhook
+  verifyIncomingWebhook,
+  discuss,
+  kickEveryone
 };
