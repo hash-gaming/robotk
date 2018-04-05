204325f3b09259d20865f052fe420fe92c12f79f
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-04-04 19:24:33 -0700

    added a logger for errors so that we can find what's wrong

--- middleware/index.js
+++ middleware/index.js
@@ -2,10 +2,12 @@ const isAdminCheck = require('./isAdmin');
 const verifyIncomingWebhook = require('./verifyWebhook');
 const discuss = require('./discuss');
 const kickEveryone = require('./kickEveryone');
+const logWhenError = require('./logWhenError');
 
 module.exports = {
   isAdminCheck,
   verifyIncomingWebhook,
   discuss,
-  kickEveryone
+  kickEveryone,
+  logWhenError
 };
